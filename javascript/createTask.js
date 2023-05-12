05032510980c1a0a28c6729ed1cd7c8c9d63e685
Elijah <eliwoodard@u.boisestate.edu>
2023-05-11 21:23:55 -0600

    Added save Script

--- javascript/createTask.js
+++ javascript/createTask.js
@@ -1,10 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var discardButton = document.querySelector(".task-button");
+  var discardButton = document.querySelector("#discard-button");
   var tasksSection = document.querySelector(".AddTaskWindow");
+  var taskContainer = document.querySelector(".Tasks");
+  var taskTitleInput = document.querySelector(".task-title");
+  var taskDescriptionTextarea = document.querySelector("#task-Description");
+  var dueDateInput = document.querySelector("#date");
+  var subjectSelect = document.querySelector("#subject-select");
+  var overlay = document.querySelector(".overlay");
+  var saveButton = document.querySelector("#save-button");
+
   tasksSection.style.display = "none";
 
   discardButton.addEventListener("click", function() {
+    console.log("Discard button clicked!");
+    taskTitleInput.value = ""; // Clear the task title input
+    taskDescriptionTextarea.value = ""; // Clear the task description textarea
+    dueDateInput.value = ""; // Reset the due date input
+    subjectSelect.value = ""; // Reset the subject dropdown
+    overlay.style.display = "none";
     tasksSection.style.display = "none";
     tasksSection.classList.remove("active");
+  });
+
+  saveButton.addEventListener("click", function() {
+    console.log("Save button clicked!");
+    var taskBox = document.createElement("div");
+    taskBox.classList.add("task-box");
+
+    var titleBox = document.createElement("div");
+    titleBox.classList.add("task-title-box");
+    titleBox.textContent = taskTitleInput.value;
+    taskContainer.appendChild(taskBox);
+taskBox.appendChild(titleBox);
+    discardButton.click();
   });
 });
--- javascript/pressCreateTask.js
+++ javascript/pressCreateTask.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   var tasksButton = document.querySelector(".create-task-button");
   var tasksSection = document.querySelector(".AddTaskWindow");
+  var overlay = document.querySelector(".overlay");
 
   tasksSection.style.display = "none";
+  overlay.style.display = "none";
 
   tasksButton.addEventListener("click", function() {
     console.log("Button clicked!");
     tasksSection.style.display = tasksSection.style.display === "none" ? "grid" : "none";
-      tasksSection.classList.toggle("active");
+    overlay.style.display = overlay.style.display === "none" ? "block" : "none";
+    tasksSection.classList.toggle("active");
   });
 });
