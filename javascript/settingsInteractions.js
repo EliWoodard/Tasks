63d8bdc76a29e1c7abbf2bf243e7514164f75816
EliWoodard <elijahwoodard136@gmail.com>
2023-07-26 21:11:41 -0600

    Added Displays to Settings

---
+++ javascript/settingsInteractions.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // buttons
+    const profileButton = document.getElementById('profile-button');
+    const filterButton = document.getElementById('filters-button');
+    const backgroundButton = document.getElementById('background-button');
+    const colorShemeButton = document.getElementById('color-button');
+
+    // displays
+    const profilePage = document.getElementById('profilePage');
+    const filterPage = document.getElementById('filterPage');
+    const backgroundPage = document.getElementById('backgroundPage');
+    const colorSchemePage = document.getElementById('colorSchemePage');
+
+    profileButton.addEventListener("click", function() {
+        // Hide all pages
+        profilePage.style.display = 'none';
+        filterPage.style.display = 'none';
+        backgroundPage.style.display = 'none';
+        colorSchemePage.style.display = 'none';
+
+        // Show profile page
+        profilePage.style.display = 'flex';
+    });
+
+    filterButton.addEventListener("click", function() {
+        // Hide all pages
+        profilePage.style.display = 'none';
+        filterPage.style.display = 'none';
+        backgroundPage.style.display = 'none';
+        colorSchemePage.style.display = 'none';
+
+        // Show filter page
+        filterPage.style.display = 'flex';
+    });
+
+    backgroundButton.addEventListener("click", function() {
+        // Hide all pages
+        profilePage.style.display = 'none';
+        filterPage.style.display = 'none';
+        backgroundPage.style.display = 'none';
+        colorSchemePage.style.display = 'none';
+
+        // Show background page
+        backgroundPage.style.display = 'flex';
+    });
+
+    colorShemeButton.addEventListener("click", function() {
+        // Hide all pages
+        profilePage.style.display = 'none';
+        filterPage.style.display = 'none';
+        backgroundPage.style.display = 'none';
+        colorSchemePage.style.display = 'none';
+
+        // Show color scheme page
+        colorSchemePage.style.display = 'flex';
+    });
+});
