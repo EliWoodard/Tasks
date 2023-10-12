b9bae553e24342948ac6062d44328cf09be46474
EliWoodard <elijahwoodard136@gmail.com>
2023-10-11 19:54:59 -0600

    Optimization for Background Settings

--- javascript/background.js
+++ javascript/background.js
@@ -21,25 +21,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const gradientCheckboxState = localStorage.getItem('gradientCheckboxState');
     const savedColor1 = localStorage.getItem('color1');
     const savedColor2 = localStorage.getItem('color2');
-
-    if (gradientCheckboxState === 'checked') {
-      document.getElementById('backgroundCheck').checked = true;
-      applyBackground(savedBackgroundValue);
-    }
-
-    if (savedColor1) {
-      document.getElementById('color1').value = savedColor1;
-    }
-
-    if (savedColor2) {
-      document.getElementById('color2').value = savedColor2;
-    }
-
+  
+    console.log("Saved Background ID:", savedBackgroundId); // Debugging
+  
+    // ... (rest of the code remains unchanged)
+  
     if (savedBackgroundId && savedBackgroundId !== 'color-gradient-background') {
       const selectedOption = document.getElementById(savedBackgroundId);
       if (selectedOption) {
         selectedOption.classList.add('selected');
         applyBackground(savedBackgroundValue);
+        
+        // Check if the fourth image was saved and adjust background position
+        if (savedBackgroundId === 'bgOption4') {
+          activeCover.style.backgroundPosition = 'bottom'; 
+        } else {
+          activeCover.style.backgroundPosition = 'center';
+        }
       }
     }
   }
@@ -85,20 +83,32 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
   });
 
-  document.querySelectorAll('.backgroundOption:not(#color-gradient-background)').forEach((element) => {
+  document.querySelectorAll('.backgroundOption:not(#color-gradient-background)').forEach((element, index) => {
     element.addEventListener('click', (e) => {
       document.getElementById('backgroundCheck').checked = false;
       localStorage.setItem('gradientCheckboxState', 'unchecked');
-
-      const newBackground = e.currentTarget.style.backgroundImage;
-      applyBackground(newBackground);
-      localStorage.setItem('backgroundOptionId', e.currentTarget.id);
-      localStorage.setItem('backgroundOptionValue', newBackground);
-      
+  
+      const regex = /smallBackground\((\d)\)\.png/;
+      const matches = e.currentTarget.style.backgroundImage.match(regex);
+      if (matches && matches[1]) {
+        const bgNumber = matches[1];
+        const newBackground = `url('Images/background(${bgNumber}).jpg')`;
+        
+        if (index === 3) { 
+          inactiveCover.style.backgroundPosition = 'bottom';
+        } else {
+          inactiveCover.style.backgroundPosition = 'center';
+        }
+        
+        applyBackground(newBackground);
+        localStorage.setItem('backgroundOptionId', e.currentTarget.id);
+        localStorage.setItem('backgroundOptionValue', newBackground);
+      }
+  
       document.querySelectorAll('.backgroundOption').forEach((el) => {
         el.classList.remove('selected');
       });
       e.currentTarget.classList.add('selected');
     });
-  });
+  }); 
 });
